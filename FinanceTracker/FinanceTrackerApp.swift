98cc29678322ee14b5f99939226defa40fe5d79b
TheFrenchyDeveloper <kevin.massol@icloud.com>
2022-10-11 09:13:28 +0200

    Initial Commit

---
+++ FinanceTracker/FinanceTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinanceTrackerApp.swift
+//  FinanceTracker
+//
+//  Created by KEVIN on 11/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct FinanceTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
