e16ee40fbdda557463168496d22bb17d37a8a5f9
TheFrenchyDeveloper <kevin.massol@icloud.com>
2022-11-27 20:16:29 +0100

    [FEAT] - Show account details when clicking on a registered account

--- FinanceTracker/HomeView.swift
+++ FinanceTracker/HomeView.swift
@@ -10,47 +10,54 @@ import SwiftUI
 struct HomeView: View {
     
     @State private var isPresentingNewAccountScreen = false
-    @StateObject private var accountList = AccountList()
+    @StateObject private var accountList = AccountList(accounts: previewAccounts)
     
     var body: some View {
-        ScrollView {
-            VStack(spacing: 32) {
-                VStack(spacing: 8) {
-                    Text("Solde total :")
-                    Text("\(String(format: "%.2F", accountList.accounts.map { $0.initialAmount }.reduce(0, +))) €")
-                        .font(.system(size: 32, weight: .bold))
-                }
-                .frame(maxWidth: .infinity)
-                
-                AccentButton(title: "+ account", color: Color("orange")) {
-                    isPresentingNewAccountScreen = true
-                }
-                
-                VStack(alignment: .leading) {
-                    Text("Mes comptes")
-                        .font(.title2)
-                        .bold()
-                    if previewAccounts.count > 0 {
-                        VStack(spacing: 16) {
-                            ForEach(accountList.accounts) { account in
-                                AccountCell(account: account)
+        NavigationView {
+            ScrollView {
+                VStack(spacing: 32) {
+                    VStack(spacing: 8) {
+                        Text("Solde total :")
+                        Text("\(String(format: "%.2F", accountList.accounts.map { $0.initialAmount }.reduce(0, +))) €")
+                            .font(.system(size: 32, weight: .bold))
+                    }
+                    .frame(maxWidth: .infinity)
+                    
+                    AccentButton(title: "+ account", color: Color("orange")) {
+                        isPresentingNewAccountScreen = true
+                    }
+                    
+                    VStack(alignment: .leading) {
+                        Text("Mes comptes")
+                            .font(.title2)
+                            .bold()
+                        if previewAccounts.count > 0 {
+                            VStack(spacing: 16) {
+                                ForEach(accountList.accounts) { account in
+                                    NavigationLink {
+                                        AccountDetailView(account: account)
+                                    } label: {
+                                        AccountCell(account: account)
+                                    }
+                                }
                             }
+                        } else {
+                            Text("Ajouter un compte")
+                                .padding(32)
+                                .frame(maxWidth: .infinity)
                         }
-                    } else {
-                        Text("Ajouter un compte")
-                            .padding(32)
-                            .frame(maxWidth: .infinity)
                     }
                 }
                 .padding(24)
             }
-        }
-        .background(Color("grey"))
-        .sheet(isPresented: $isPresentingNewAccountScreen) {
-            AccountCreationView { newAccount in
-                accountList.accounts.append(newAccount)
+            .background(Color("grey"))
+            .sheet(isPresented: $isPresentingNewAccountScreen) {
+                AccountCreationView { newAccount in
+                    accountList.accounts.append(newAccount)
+                }
             }
         }
+        .accentColor(.black)
     }
 }
 
